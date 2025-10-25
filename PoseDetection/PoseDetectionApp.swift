00ce055b9738c0063c3e4a6a1738acf7f8316ea4
Robin <emeka14real@gmail.com>
2025-10-25 11:21:00 +0100

    Initial Commit

---
+++ PoseDetection/PoseDetectionApp.swift
@@ -0,0 +1,17 @@
+//
+//  PoseDetectionApp.swift
+//  PoseDetection
+//
+//  Created by Igboanyika Nnaemeka on 25/10/2025.
+//
+
+import SwiftUI
+
+@main
+struct PoseDetectionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PoseDetectionTests/PoseDetectionTests.swift
@@ -0,0 +1,17 @@
+//
+//  PoseDetectionTests.swift
+//  PoseDetectionTests
+//
+//  Created by Igboanyika Nnaemeka on 25/10/2025.
+//
+
+import Testing
+@testable import PoseDetection
+
+struct PoseDetectionTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
